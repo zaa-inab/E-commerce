cae08f038514f86dd079df99d202bcbd6f3111b9
zaa-inab <chakroun.zainab.ismo2020@gmail.com>
2022-06-07 16:03:51 +0100

    Project Structure, Routing and UI design Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './User/products/components/all-products/all-products.component';
+import { ProductsDetailsComponent } from './User/products/components/products-details/products-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:' ' , redirectTo:'products' ,pathMatch:'full'},
+  {path:'products' ,component: AllProductsComponent},
+  {path:'details' , component:ProductsDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
